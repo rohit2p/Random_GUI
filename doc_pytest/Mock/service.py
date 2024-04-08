21d2e261d06f67d9cdfb944e1e1e770d1316080d
Rohit prasad <rohitprasad20022@gmail.com>
2024-04-08 23:41:18 +0530

    pytest and other file

--- doc_pytest/Mock/service.py
+++ doc_pytest/Mock/service.py
@@ -1,3 +1,5 @@
+import requests
+
 database = {
     1 : "Rohit",
     2 : "Ram"
@@ -7,3 +9,10 @@ def get_user_from_id(user_id):
     # return print(database.get(user_id))
     return database.get(user_id)
 # get_user_from_id(2)
+
+def get_user_through_api():
+    responce = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    if responce.status_code == 200:
+        return responce.json()
+
+    raise requests.HTTPError
--- test/Test_mock/test_source.py
+++ test/Test_mock/test_source.py
@@ -1,10 +1,34 @@
 import unittest.mock as mock
-import doc_pytest.Mock.service as service
+import requests
+import Random_GUI.doc_pytest.Mock.service as service
 
 
-@mock.patch("doc_pytest.Mock.service.get_user_from_id")
+@mock.patch("Random_GUI.doc_pytest.Mock.service.get_user_from_id")
 def test_get_user_by_id(mock_get_user_from_db):
     mock_get_user_from_db.return_value = "Mocked Alice"
     user_name = service.get_user_from_id(1)
 
     assert user_name == "Mocked Alice"
+
+"""test function using unittest.mock to mock the behavior of an API request using the requests library"""
+@mock.patch("requests.get")
+def test_get_user_through_api(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "rohit prasad"}
+    mock_get.return_value = mock_response
+    data = service.get_user_through_api()
+    assert data == {"id": 1, "name": "rohit prasad"}
+
+"""
+In the test code test_get_user_through_api, we put mock_response into mock_get because mock_get is a mock object that 
+simulates the requests.get function. By assigning mock_response to mock_get.return_value, we are configuring the mock 
+object to return mock_response whenever requests.get is called within the get_user_through_api function.
+"""
+
+"""
+When the test runs and get_user_through_api is called, instead of making a real HTTP request to 
+https://jsonplaceholder.typicode.com/todos/1, the patched requests.get function (represented by mock_get) 
+returns mock_response.This allows the test to control the response of the API call and verify that get_user_through_api
+ handles the response correctly.
+"""
