4a45d4ff7a1f8c4348e2b52f9037f3671a738817
rohit <rohitprasad20022@gmail.com>
2024-05-16 00:20:24 +0530

    sj

---
+++ TO do application /main.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem
+from PyQt5 import QtCore
+from PyQt5.uic import loadUi
+import sys
+
+tasks = ["work on project", "watch tutorial", "go to birthday"]
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        loadUi("main.ui", self)
+        self.calendarWidget.selectionChanged.connect(self.calanderdateChanged)
+        self.updateTaskList()
+
+    def calanderdateChanged(self):
+        dateSelected = self.calendarWidget.selectedDate().toPyDate()
+        print("selected date: ", dateSelected)
+
+    def updateTaskList(self):
+        for task in tasks:
+            item = QListWidgetItem(task)
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.taskslistWidget.addItem(item)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
